81af743d115a3d6f8bfdf3f38ba0026a6ac8e3ea
yinian3shuhongwentangniaoxizaogou <shangxiyue3232@126.com>
2024-05-08 19:26:53 +0800

    新建ReminderPopUp，修改App.vue，均为深夜提醒

--- view/src/App.vue
+++ view/src/App.vue
@@ -3,3 +3,34 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import ReminderPopUp from "@/views/home/ReminderPopUp.vue";
+
+export default {
+  components: {
+    ReminderPopUp
+  },
+  data() {
+    return {
+      showReminder: false
+    };
+  },
+  created() {
+    this.setupReminder();
+  },
+  methods:{
+    setupReminder() {
+      const checkInterval = setInterval(() => {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        if (hours === 23 && minutes === 0) {
+          this.setupReminder = true;
+          clearInterval(checkInterval);
+        }
+      }, 60000);
+    }
+  }
+}
+</script>
---
+++ view/src/views/home/ReminderPopUp.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: ['visible']
+}
+</script>
+
+<template>
+  <div v-if="visible" class="reminder-popup">
+    主人主人，已经很晚了，注意保持健康哦！
+  </div>
+</template>
+
+<style>
+.reminder-popup {
+  position: fixed;
+  top: 80%;
+  right: 20%;
+  transform: translateX(-50%);
+  padding: 20px;
+  background-color: white;
+  border: black;
+  z-index: 1000;
+}
+</style>
