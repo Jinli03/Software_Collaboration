2bb53c15f058b291db9dd9198baa8a7c8d4fa4ef
yinian3shuhongwentangniaoxizaogou <shangxiyue3232@126.com>
2024-05-20 22:40:07 +0800

    修饰页面变得更好看

--- view/src/views/home/Password.vue
+++ view/src/views/home/Password.vue
@@ -4,17 +4,17 @@
 *@date 2024/2/21 22:02
 -->
 <template>
-  <div>
-    <el-card style="width: 50%">
-      <el-form ref="formRef" :model="user" :rules="rules" label-width="80px" style="padding-right: 20px">
-        <el-form-item label="originPassword" prop="password">
-          <el-input show-password v-model="user.password" placeholder="null" disabled></el-input>
+  <div class="center-container">
+    <el-card style="width: 50%; max-width: 600px;">
+      <el-form ref="formRef" :model="user" :rules="rules" label-width="100px">
+        <el-form-item label="原始密码" prop="password">
+          <el-input show-password v-model="user.password" placeholder="请输入原始密码"></el-input>
         </el-form-item>
-        <el-form-item label="updatePassword" prop="newPassword">
-          <el-input show-password v-model="user.newPassword" placeholder="null"></el-input>
+        <el-form-item label="新密码" prop="newPassword">
+          <el-input show-password v-model="user.newPassword" placeholder="请输入新密码"></el-input>
         </el-form-item>
-        <el-form-item label="confirmPassword" prop="confirmPassword">
-          <el-input show-password v-model="user.confirmPassword" placeholder="null"></el-input>
+        <el-form-item label="确认新密码" prop="confirmPassword">
+          <el-input show-password v-model="user.confirmPassword" placeholder="确认新密码"></el-input>
         </el-form-item>
         <div style="text-align: center">
           <el-button type="primary" @click="update">修改</el-button>
@@ -24,58 +24,92 @@
   </div>
 </template>
 
+
 <script>
 export default {
   name: "Password",
-  props: {},
-  components: {},
   data() {
     const validatePassword = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('请确认密码'));
-      } else if (value!== this.user.newPassword){
-        callback(new Error('确认密码错误'));
-      } else{
-        callback()
+      } else if (value !== this.user.newPassword) {
+        callback(new Error('两次输入的密码不一致'));
+      } else {
+        callback();
       }
-    }
+    };
+
     return {
       user: JSON.parse(localStorage.getItem('pilot') || '{}'),
       rules: {
         password: [
-          {required: true, message: '请输入原始密码', trigger: 'blur'}
+          { required: true, message: '请输入原始密码', trigger: 'blur' }
         ],
         newPassword: [
-          {required: true, message: '请输入修改密码', trigger: 'blur'}
+          { required: true, message: '请输入新密码', trigger: 'blur' }
         ],
         confirmPassword: [
-          {validator: validatePassword, required: true, trigger: 'blur'}
+          { validator: validatePassword, required: true, trigger: 'blur' }
         ],
       }
-    }
+    };
   },
-  mounted: {},
   methods: {
     update() {
       this.$refs.formRef.validate((valid) => {
         if (valid) {
-          this.user.password = this.user.newPassword
-          this.$request.put('/user/update', this.user).then(res =>{
+          this.user.password = this.user.newPassword;
+          this.$request.put('/user/update', this.user).then(res => {
             if (res.code === '200') {
-              this.$message.success('更新成功')
-              this.$router.push('/login')
+              this.$message.success('密码更新成功');
+              this.$router.push('/login');
             } else {
-              this.$message.error(res.msg)
+              this.$message.error(res.msg);
             }
-          })
+          });
         }
-      })
+      });
     }
   },
 }
 </script>
 
-<style>
+
+<style scoped>
+.center-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+
+.el-card {
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  border-radius: 10px;
+  padding: 20px;
+}
+
+.el-input__inner, .el-button {
+  border-radius: 5px;
+}
+
+.el-input__inner:focus {
+  border-color: #409EFF;
+  box-shadow: 0 0 8px rgba(64, 158, 255, 0.2);
+}
+
+.el-button {
+  background-color: #409EFF;
+  color: white;
+  transition: background-color 0.2s;
+}
+
+.el-button:hover {
+  background-color: #337ecc;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+}
+
 .el-form-item__label {
   font-weight: bold;
 }
