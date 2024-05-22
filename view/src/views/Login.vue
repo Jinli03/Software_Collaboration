4288b0557c3ec2d46424b6f17b4247bddb1bb110
yinian3shuhongwentangniaoxizaogou <shangxiyue3232@126.com>
2024-05-22 15:22:02 +0800

    优化登录页面

--- view/src/views/Login.vue
+++ view/src/views/Login.vue
@@ -1,57 +1,49 @@
 <template>
-  <div style="height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: #0f98">
-    <div style="display: flex; background-color: white; width: 50%; border-radius: 10px; overflow: hidden">
-      <div style="flex: 1;">
-        <img src="@/assets/login.png" style="width: 100%">
+  <div class="login-page">
+    <div class="login-container">
+      <div class="login-image">
+        <img src="@/assets/login.png" alt="login">
       </div>
-      <div style="flex: 1; display: flex; align-items: center; justify-content: center">
+      <div class="form-container">
         <el-form :model="user" :rules="rules" ref="loginRef">
-          <div style="font-size: 20px; font-weight: bold; text-align: center; margin-bottom: 20px; margin-top: 20px">欢迎登录考研分析局</div>
-          <el-form-item prop="name" style="width: 90%">
+          <div class="form-header">欢迎登录考研分析局</div>
+          <el-form-item prop="name" class="form-item">
             <el-input prefix-icon="el-icon-user" size="medium" placeholder="请输入账号" v-model="user.name"></el-input>
           </el-form-item>
-          <el-form-item prop="password" style="width: 90%">
+          <el-form-item prop="password" class="form-item">
             <el-input prefix-icon="el-icon-lock" size="medium" show-password placeholder="请输入密码" v-model="user.password"></el-input>
           </el-form-item>
-          <el-form-item prop="code" style="width: 100%">
-            <div style="display: flex">
-              <div>
-                <el-input prefix-icon="el-icon-check" size="medium" placeholder="请输入验证码(小写)" style="flex: 1; width: 80%" v-model="user.code"></el-input>
-              </div>
-              <div style="flex: 1; height: 36px; margin-right: 30px" >
-                <valid-code @update:value="getCode"></valid-code>
-              </div>
+          <el-form-item prop="code" class="form-item">
+            <div class="code-container">
+              <el-input prefix-icon="el-icon-check" size="medium" placeholder="请输入验证码(小写)" v-model="user.code"></el-input>
+              <valid-code @update:value="getCode" class="valid-code"></valid-code>
             </div>
           </el-form-item>
-          <el-form-item style="width: 100%">
-            <el-button @click="login" type="success" style="margin-top: 40px">登录</el-button>
-            <div style="display: flex; margin-top: 30px;">
-              <div style="flex: 2">
-                还没有账号？请<span style="color: dodgerblue; cursor: pointer" @click="$router.push('/register')"> 注册</span>
-              </div>
-              <div style="flex: 1; width: 50%; align-items: center">
-                <el-button type="text" style="cursor: pointer" @click="handleForgetPass()">忘记密码</el-button>
-                <el-dialog title="忘记密码" :visible.sync="forgetPassDialogVis">
-                  <el-form :model="forgetUserForm">
-                    <el-form-item label="用户名" :label-width="formLabelWidth">
-                      <el-input v-model="forgetUserForm.name" autocomplete="off" placeholder="请输入用户名"></el-input>
-                    </el-form-item>
-                    <el-form-item label="手机号" :label-width="formLabelWidth">
-                      <el-input v-model="forgetUserForm.phone" autocomplete="off" placeholder="请输入手机号"></el-input>
-                    </el-form-item>
-                  </el-form>
-                  <div slot="footer" class="dialog-footer">
-                    <el-button @click="forgetPassDialogVis = false">取 消</el-button>
-                    <el-button type="primary" @click="resetPassword">确 定</el-button>
-                  </div>
-                </el-dialog>
-              </div>
+          <el-form-item class="form-actions">
+            <el-button @click="login" type="success" class="login-button">登录</el-button>
+            <div class="extra-actions">
+              <div>还没有账号？请<span class="register-link" @click="$router.push('/register')"> 注册</span></div>
+              <div class="forgot-password" @click="handleForgetPass">忘记密码点这里</div>
+              <el-dialog title="忘记密码" :visible.sync="forgetPassDialogVis">
+                <el-form :model="forgetUserForm">
+                  <el-form-item label="用户名" :label-width="formLabelWidth">
+                    <el-input v-model="forgetUserForm.name" autocomplete="off" placeholder="请输入用户名"></el-input>
+                  </el-form-item>
+                  <el-form-item label="手机号" :label-width="formLabelWidth">
+                    <el-input v-model="forgetUserForm.phone" autocomplete="off" placeholder="请输入手机号"></el-input>
+                  </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                  <el-button @click="forgetPassDialogVis = false">取消</el-button>
+                  <el-button type="primary" @click="resetPassword">确定</el-button>
+                </div>
+              </el-dialog>
             </div>
           </el-form-item>
         </el-form>
       </div>
     </div>
-    <div style="text-align: center; padding: 10px; color: white; ">
+    <div class="footer">
       &copy; 2024 @Jinli @hfy | 考研分析局.
     </div>
   </div>
@@ -69,9 +61,9 @@ export default {
     const validateCode = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('请输入验证码'));
-      } else if (value.toLowerCase() !== this.code){
+      } else if (value.toLowerCase() !== this.code) {
         callback(new Error('验证码错误'));
-      } else{
+      } else {
         callback()
       }
     }
@@ -94,16 +86,10 @@ export default {
           {required: true, message: '请输入密码', trigger: 'blur'}
         ],
         code: [
-          { validator: validateCode, trigger: 'blur' }
+          {validator: validateCode, trigger: 'blur'}
         ]
       }
     }
-  },
-  created() {
-
-  },
-  mounted() {
-
   },
   methods: {
     handleForgetPass() {
@@ -111,8 +97,8 @@ export default {
       this.forgetPassDialogVis = true
     },
     resetPassword() {
-      this.$request.put('/password', this.forgetUserForm).then(res =>{
-        if (res.code === '200'){
+      this.$request.put('/password', this.forgetUserForm).then(res => {
+        if (res.code === '200') {
           this.$message.success('重置成功')
         } else {
           this.$message.error(res.msg)
@@ -120,15 +106,14 @@ export default {
       })
     },
     getCode(code) {
-      console.log(code)
       this.code = code.toLowerCase()
     },
     login() {
       // 拦截请求
       this.$refs['loginRef'].validate((valid) => {
         if (valid) {
-          this.$request.post("/login", this.user).then(res =>{
-            if (res.code === '200'){
+          this.$request.post("/login", this.user).then(res => {
+            if (res.code === '200') {
               this.$router.push('/')
               this.$message.success('登陆成功')
               localStorage.setItem("pilot", JSON.stringify(res.data))
@@ -137,16 +122,123 @@ export default {
             }
           })
         } else {
-          console.log('error submit!!');
           return false;
         }
       });
     },
   }
 }
-
 </script>
 
 <style scoped>
+.login-page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #0f98;
+}
 
+.login-container {
+  display: flex;
+  background-color: white;
+  width: 50%;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.login-image {
+  flex: 1;
+}
+
+.login-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.form-container {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+
+.form-header {
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin: 20px 0;
+}
+
+.form-item {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.code-container {
+  display: flex;
+  align-items: center;
+}
+
+.valid-code {
+  flex: 1;
+  height: 50px;
+  margin-left: 10px;
+}
+
+.form-actions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.login-button {
+  width: 200px;
+  height: 50px;
+  line-height: 25px;
+  background-color: #f5f5f5;
+  color: #333;
+  border: 1px solid #ccc;
+  transition: background-image 0.3s, color 0.3s;
+}
+
+.login-button:hover {
+  background-image: linear-gradient(to right, #44c767, #5cbf2a);
+  color: white;
+}
+
+.extra-actions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+}
+
+.register-link {
+  color: dodgerblue;
+  cursor: pointer;
+}
+
+.forgot-password {
+  text-align: center;
+  color: #888;
+  font-size: 14px;
+  cursor: pointer;
+  margin-top: 20px;
+  transition: text-decoration 0.3s;
+}
+
+.forgot-password:hover {
+  text-decoration: underline;
+}
+
+.footer {
+  text-align: center;
+  padding: 10px;
+  color: white;
+}
 </style>
