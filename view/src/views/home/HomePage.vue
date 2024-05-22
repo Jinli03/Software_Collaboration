128ac840a32d6a8c1d502b76e0fc95e9b3be15f4
yinian3shuhongwentangniaoxizaogou <shangxiyue3232@126.com>
2024-05-22 15:46:02 +0800

    优化页面

--- view/src/views/home/HomePage.vue
+++ view/src/views/home/HomePage.vue
@@ -1,27 +1,24 @@
 <template>
-  <div>
-    <div class="background-image-container">
-      <div>
+  <div class="background-image-container">
+    <div class="content-container">
+      <div class="image-container">
         <div>
-          <div>
-            <img src="../../assets/222.png" alt="First Image" class="image1" style="width: 200px; height: 200px; position: absolute; top: 480px; left: 400px;" @click="$router.push('search')">
-            <p class="unselectable" style="position: absolute; top: 650px; left: 430px; font-size: 26px; color: black">分析局</p>
-          </div>
-          <div>
-            <img src="@/assets/333.png" alt="Second Image" class="image2" style="width: 200px; height: 200px; position: absolute; bottom: 480px; right: 320px" @click="$router.push('personal')">
-            <p class="unselectable" style="position: absolute; bottom: 430px; right: 380px; font-size: 26px; color: black">个人档案局</p>
-          </div>
-          <div>
-            <img src="@/assets/444.png" alt="Third Image" class="image3" style="width: 200px; height: 200px; position: absolute; bottom: 470px; left: 300px" @click="$router.push('exchange')">
-            <p class="unselectable" style="position: absolute; bottom: 420px; left: 330px; font-size: 26px; color: black">图书交易局</p>
-          </div>
+          <img src="../../assets/222.png" alt="First Image" class="image image1" @click="$router.push('search')">
+          <p class="image-label" style="top: 650px; left: 430px;">分析局</p>
         </div>
-        <div class="watermark">&copy; Designed by Agency.Icons by icons8</div>
+        <div>
+          <img src="@/assets/333.png" alt="Second Image" class="image image2" @click="$router.push('personal')">
+          <p class="image-label" style="bottom: 430px; right: 380px;">个人档案局</p>
+        </div>
+        <div>
+          <img src="@/assets/444.png" alt="Third Image" class="image image3" @click="$router.push('exchange')">
+          <p class="image-label" style="bottom: 420px; left: 330px;">图书交易局</p>
+        </div>
       </div>
+      <div class="watermark">&copy; Designed by Agency. Icons by icons8</div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -33,82 +30,73 @@ export default {
 </script>
 
 <style scoped>
-
 .watermark {
   position: fixed;
-  bottom: 40px; /* 距离底部的距离 */
-  left: 20px; /* 距离左侧的距离 */
-  font-size: 20px; /* 水印字体大小 */
-  color: rgba(0, 0, 0, 0.1); /* 水印字体颜色 */
-  z-index: 9999; /* 确保水印在其他元素之上 */
+  bottom: 40px;
+  left: 20px;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+  user-select: none;
 }
-
 
 .background-image-container {
   background-image: url('@/assets/background/homepage.png');
-  background-size: 90%; /* 使用 cover 让背景图片填满整个容器 */
-  background-position: center; /* 图片居中显示 */
-  height: 100vh; /* 使用视口高度作为容器高度 */
-  width: 100vw; /* 使用视口宽度作为容器宽度 */
+  background-size: 90%;
+  background-position: center;
+  height: 100vh;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: white; /* 文字颜色 */
+  color: white;
 }
 
-
-/* 针对内容的额外样式 */
-.background-image-container > div {
+.content-container {
   text-align: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 
-.background-image-container h1 {
-  font-size: 2em;
-  margin-bottom: 10px;
+.image-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 
-.background-image-container p {
-  font-size: 1.2em;
+.image {
+  width: 200px;
+  height: 200px;
+  transition: transform 0.3s ease-out;
 }
 
 .image1 {
   position: absolute;
-  top: 500px;
-  left: 350px;
-  transition: transform 0.3s ease-out;
+  top: 480px;
+  left: 400px;
 }
 
 .image2 {
-  transition: transform 0.3s ease-out;
-}
-
-.image1:hover {
-  transform: scale(1.1); /* 放大1.1倍 */
-}
-
-.image2:hover {
-  transform: scale(1.1); /* 放大1.1倍 */
+  position: absolute;
+  bottom: 480px;
+  right: 320px;
 }
 
 .image3 {
-  transition: transform 0.3s ease-out;
+  position: absolute;
+  bottom: 470px;
+  left: 300px;
 }
 
-.image3:hover {
-  transform: scale(1.1); /* 放大1.1倍 */
+.image:hover {
+  transform: scale(1.1);
 }
 
-.unselectable {
-  user-select: none; /* 防止文本被选中 */
-}
-
-/* 针对每个图像的hover效果和其他已定义的样式 */
-.image1:hover, .image2:hover, .image3:hover {
-  transform: scale(1.1); /* 鼠标悬浮时放大 */
-}
-
-/* 防止水印被选中，如果需要的话 */
-.watermark {
+.image-label {
+  position: absolute;
+  font-size: 26px;
+  color: black;
   user-select: none;
 }
 </style>
